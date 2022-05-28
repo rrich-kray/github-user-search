03d593715a3c1d7d0a9728415a00c0b1e74193f5
rrich-kray <rrich.kray@gmail.com>
2022-05-28 15:56:08 -0400

    created basic formatting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [currentUser, changeCurrentUser] = useState("");
+  const [search, updateSearch] = useState("");
+  const api = `https://api.github.com/users/${search}`;
+
+  const fetchUser = async () => {
+    const res = await fetch(api);
+    res.json().then((userObj) => {
+      console.log(userObj);
+      changeCurrentUser(userObj);
+      console.log(currentUser);
+    });
+  };
+
+  const handleInputChange = (e) => {
+    updateSearch(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <div id="content-container">
+        <div id="info-bar"></div>
+        <div id="search-bar">
+          <input
+            onChange={handleInputChange}
+            placeholder="Enter GitHub username..."
+          ></input>
+          <button id="search-btn" onClick={fetchUser}>
+            Search
+          </button>
+        </div>
+        <div id="user-info-container">
+          <img id="avatar" src={currentUser.avatar_url} alt="avatar"></img>
+          <h1 id="username">{currentUser.login}</h1>
+        </div>
+      </div>
     </div>
   );
 }
